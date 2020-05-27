a6c132bbba9198db54f7a3b573630acd455e4227
niknap <nikoknapp@posteo.de>
2020-05-27 22:07:14 +0200

    Add package name to functions

--- R/periodic.raster.frame_function.R
+++ R/periodic.raster.frame_function.R
@@ -12,50 +12,50 @@
 #' @author Nikolai Knapp, nikolai.knapp@ufz.de
 
 periodic.raster.frame <- function(ras, w=10){
-  require(raster)
-  
+  #require(raster)
+
   # Memorize extent and resolution of input raster
-  minx <- xmin(ras)
-  maxx <- xmax(ras)
-  miny <- ymin(ras)
-  maxy <- ymax(ras)
-  xres <- res(ras)[1]
-  yres <- res(ras)[2]
+  minx <- raster::xmin(ras)
+  maxx <- raster::xmax(ras)
+  miny <- raster::ymin(ras)
+  maxy <- raster::ymax(ras)
+  xres <- raster::res(ras)[1]
+  yres <- raster::res(ras)[2]
 
   # Increase the extent by the desired frame
-  new.extent <- extent(minx-xres*w, maxx+xres*w, miny-yres*w, maxy+yres*w)
-  new.ras <- extend(ras, new.extent)
+  new.extent <- raster::extent(minx-xres*w, maxx+xres*w, miny-yres*w, maxy+yres*w)
+  new.ras <- raster::extend(ras, new.extent)
 
   # Subset the right, left, lower and upper frame raster
-  left.ras <- crop(ras, extent(minx, minx+w*xres, miny, maxy))
-  right.ras <- crop(ras, extent(maxx-xres*w, maxx, miny, maxy))
-  lower.ras <- crop(ras, extent(minx, maxx, miny, miny+w*yres))
-  upper.ras <- crop(ras, extent(minx, maxx, maxy-yres*w, maxy))
-  
+  left.ras <- raster::crop(ras, extent(minx, minx+w*xres, miny, maxy))
+  right.ras <- raster::crop(ras, extent(maxx-xres*w, maxx, miny, maxy))
+  lower.ras <- raster::crop(ras, extent(minx, maxx, miny, miny+w*yres))
+  upper.ras <- raster::crop(ras, extent(minx, maxx, maxy-yres*w, maxy))
+
   # Shift them
-  left.ras <- setExtent(left.ras, extent(minx+maxx, maxx+w*xres, miny, maxy))
-  right.ras <- setExtent(right.ras, extent(minx-w*xres, minx, miny, maxy))
-  lower.ras <- setExtent(lower.ras, extent(minx, maxx, miny+maxy, maxy+w*yres))
-  upper.ras <- setExtent(upper.ras, extent(minx, maxx, miny-w*yres, miny))
-  
+  left.ras <- raster::setExtent(left.ras, extent(minx+maxx, maxx+w*xres, miny, maxy))
+  right.ras <- raster::setExtent(right.ras, extent(minx-w*xres, minx, miny, maxy))
+  lower.ras <- raster::setExtent(lower.ras, extent(minx, maxx, miny+maxy, maxy+w*yres))
+  upper.ras <- raster::setExtent(upper.ras, extent(minx, maxx, miny-w*yres, miny))
+
   # Mosaic them
-  new.ras <- mosaic(ras, left.ras, right.ras, lower.ras, upper.ras, fun=mean)
-  
+  new.ras <- raster::mosaic(ras, left.ras, right.ras, lower.ras, upper.ras, fun=mean)
+
   # Subset the corners
-  ll.ras <- crop(ras, extent(minx, minx+w*xres, miny, miny+w*yres))
-  lr.ras <- crop(ras, extent(maxx-w*xres, maxx, miny, miny+w*yres))
-  ul.ras <- crop(ras, extent(minx, minx+w*xres, maxy-w*yres, maxy))
-  ur.ras <- crop(ras, extent(maxx-w*xres, maxx, maxy-w*yres, maxy))
-  
+  ll.ras <- raster::crop(ras, extent(minx, minx+w*xres, miny, miny+w*yres))
+  lr.ras <- raster::crop(ras, extent(maxx-w*xres, maxx, miny, miny+w*yres))
+  ul.ras <- raster::crop(ras, extent(minx, minx+w*xres, maxy-w*yres, maxy))
+  ur.ras <- raster::crop(ras, extent(maxx-w*xres, maxx, maxy-w*yres, maxy))
+
   # Shift them
-  ll.ras <- setExtent(ll.ras, extent(maxx, maxx+w*xres, maxy, maxy+w*yres))
-  lr.ras <- setExtent(lr.ras, extent(minx-w*xres, minx, maxy, maxy+w*yres))
-  ul.ras <- setExtent(ul.ras, extent(maxx, maxx+w*xres, miny-w*yres, miny))
-  ur.ras <- setExtent(ur.ras, extent(minx-w*xres, minx, miny-w*yres, miny))
-  
+  ll.ras <- raster::setExtent(ll.ras, extent(maxx, maxx+w*xres, maxy, maxy+w*yres))
+  lr.ras <- raster::setExtent(lr.ras, extent(minx-w*xres, minx, maxy, maxy+w*yres))
+  ul.ras <- raster::setExtent(ul.ras, extent(maxx, maxx+w*xres, miny-w*yres, miny))
+  ur.ras <- raster::setExtent(ur.ras, extent(minx-w*xres, minx, miny-w*yres, miny))
+
   # Mosaic them
-  new.ras <- mosaic(new.ras, ll.ras, lr.ras, ul.ras, ur.ras, fun=mean)
- 
+  new.ras <- raster::mosaic(new.ras, ll.ras, lr.ras, ul.ras, ur.ras, fun=mean)
+
   # plot(new.ras)
   return(new.ras)
 }
