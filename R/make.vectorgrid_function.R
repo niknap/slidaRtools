a4ef3f4349eef37b729a2016e1e980522f2d70af
niknap <nikoknapp@posteo.de>
2020-06-10 11:44:45 +0200

    Change ID ordering from column-wise to row-wise

--- R/make.vectorgrid_function.R
+++ R/make.vectorgrid_function.R
@@ -11,8 +11,14 @@
 #' @return SpatialPolygonsDataFrame
 #' @keywords vector grid shape fishnet sampling matrix lattice polygon
 #' @export
-#' @examples testgrid <- make.vectorgrid(ll.x=0, ll.y=0, size.x=2, size.y=3, n.x=10, n.y=5)
-#' plot(testgrid)
+#' @examples testgrid.spdf <- make.vectorgrid(ll.x=0, ll.y=0, size.x=2, size.y=3, n.x=10, n.y=5)
+#' plot(testgrid.spdf)
+#' sub.spdf <- subset(testgrid, ID==1)
+#' plot(sub.spdf, add=T, col="red")
+#' sub.spdf <- subset(testgrid, ID==2)
+#' plot(sub.spdf, add=T, col="green")
+#' sub.spdf <- subset(testgrid, ID==11)
+#' plot(sub.spdf, add=T, col="blue")
 
 make.vectorgrid <- function(ll.x=0, ll.y=0, size.x=1, size.y=1, n.x=1, n.y=1, rot.angle=0){
   require(sp)
@@ -21,41 +27,37 @@ make.vectorgrid <- function(ll.x=0, ll.y=0, size.x=1, size.y=1, n.x=1, n.y=1, ro
   ps.list <- list()
   # Loop over all unique polygons and process the coordinate info to build a polygon
   i <- 1
-  for(i.x in 1:n.x){
-    # Calculate x-coordinates of all 4 corners
-    my.ll.x <- ll.x + i.x * size.x - size.x
-    my.ul.x <- ll.x + i.x * size.x - size.x
-    my.ur.x <- ll.x + i.x * size.x
-    my.lr.x <- ll.x + i.x * size.x
-    for(i.y in 1:n.y){
-      # Calculate y-coordinates of all 4 corners
-      my.ll.y <- ll.y + i.y * size.y - size.y
-      my.lr.y <- ll.y + i.y * size.y - size.y
-      my.ul.y <- ll.y + i.y * size.y
-      my.ur.y <- ll.y + i.y * size.y
+  for(i.y in 1:n.y){
+    # Calculate y-coordinates of all 4 corners
+    my.ll.y <- ll.y + i.y * size.y - size.y
+    my.lr.y <- ll.y + i.y * size.y - size.y
+    my.ul.y <- ll.y + i.y * size.y
+    my.ur.y <- ll.y + i.y * size.y
+    for(i.x in 1:n.x){
+      # Calculate x-coordinates of all 4 corners
+      my.ll.x <- ll.x + i.x * size.x - size.x
+      my.ul.x <- ll.x + i.x * size.x - size.x
+      my.ur.x <- ll.x + i.x * size.x
+      my.lr.x <- ll.x + i.x * size.x
       # Build a corner matrix
       corner.mx <- matrix(c(my.ll.x, my.ll.y, my.ul.x, my.ul.y,
                             my.ur.x, my.ur.y, my.lr.x, my.lr.y,
                             my.ll.x, my.ll.y), ncol=2, byrow=T)
       # Make a polygon
-      p = Polygon(corner.mx)
-      ps = Polygons(list(p), 1)
+      p = sp::Polygon(corner.mx)
+      ps = sp::Polygons(list(p), 1)
       # Add it to the Polygons list (by default IDs > 1e5 are written in scientific
       # notation; this is avoided with format function; this is needed for later
       # conversion to SpatialPolygonsDataFrame)
-      ps.list[[i]] <- Polygons(list(p), ID=format(i, scientific=F))
+      ps.list[[i]] <- sp::Polygons(list(p), ID=format(i, scientific=F))
       i <- i+1
     }
   }
   # Convert them to spatial polygons
-  sps <- SpatialPolygons(ps.list)
+  sps <- sp::SpatialPolygons(ps.list)
   # Convert them to a spatial polygon dataframe
-  spdf <- SpatialPolygonsDataFrame(sps, data.frame(ID=1:(i-1)))
+  spdf <- sp::SpatialPolygonsDataFrame(sps, data.frame(ID=1:(i-1)))
   # Rotate the spatial polygons dataframe
-  spdf <- elide(spdf, rotate=rot.angle)
+  spdf <- maptools::elide(spdf, rotate=rot.angle)
   return(spdf)
 }
-
-
-
-
